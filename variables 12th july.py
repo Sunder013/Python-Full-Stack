0ecd292a25e582052adc75817fde0bebc1996716
Akula Syam Sunder <117805550+Sunder013@users.noreply.github.com>
2026-07-12 13:30:30 +0530

    Add files via upload

---
+++ variables 12th july.py
@@ -0,0 +1,92 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+variables
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    variables
+NameError: name 'variables' is not defined
+#Variables
+print(6+8)
+14
+a=4
+b=9
+print(a+b)
+13
+c=30
+print(C)
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    print(C)
+NameError: name 'C' is not defined. Did you mean: 'c'?
+print(c)
+30
+X = 50
+print(X)
+50
+c = (10,20,30)
+print(c)
+(10, 20, 30)
+v = "Shyam"
+print(v)
+Shyam
+city = "vja")
+SyntaxError: unmatched ')'
+city = "vja"
+print(city)
+vja
+G = "India"
+print(G)
+India
+a2 = 30
+print(a2)
+30
+print(2a)
+SyntaxError: invalid decimal literal
+a0123456789 = 100
+print(a0123456789)
+100
+@ = 50
+SyntaxError: invalid syntax
+_=30
+print(_)
+30
+first name ="pooja"
+SyntaxError: invalid syntax
+>>> first_name = "shyam"
+>>> print(first_name)
+shyam
+>>> firstname = "Sam"
+>>> print(firstname)
+Sam
+>>> a=4,b=5
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+>>> a=4;b=9
+>>> print(a+b)
+13
+>>> a,b = 5,6
+>>> print(a+b)
+11
+>>> a =[10,20,30]
+>>> print(a)
+[10, 20, 30]
+>>> a,b,c = 10,20,30
+>>> print(a,b,c)
+10 20 30
+>>> a,b,c =10,20
+Traceback (most recent call last):
+  File "<pyshell#43>", line 1, in <module>
+    a,b,c =10,20
+ValueError: not enough values to unpack (expected 3, got 2)
+>>> a,b,c = (6,7,8)
+>>> print(a,b,c)
+6 7 8
+>>> 1name = "Shyam"
+SyntaxError: invalid decimal literal
+>>> lname = "Shyam"
+>>> fname = "Sunder"
+>>> print(lname+fname)
+ShyamSunder
+>>> print(lname+" "+fname)
+Shyam Sunder
+>>> print(lname,fname)
+Shyam Sunder
