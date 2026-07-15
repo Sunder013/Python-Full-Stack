c48e11cd8e692f33a26b6985e523436876d6ee37
Akula Syam Sunder <117805550+Sunder013@users.noreply.github.com>
2026-07-15 15:49:53 +0530

    Add files via upload

---
+++ tasks on july 15.py
@@ -0,0 +1,57 @@
+'''a = 10
+b = 20
+a,b=b,a
+print("a value is",a)
+print("b value is",b)'''
+
+'''a = 10
+b = 20
+a = a+b
+b = a-b
+a = a-b
+print("after swapping a=%d,b=%d" %(a,b))'''
+
+'''a = 45
+b = 64
+a = a+b
+b = a-b
+a = a-b
+print("after swapping a=%d,b=%d" %(a,b))'''
+
+'''a =["codegnan","python","course"]
+b = str(a)
+print(b.upper())'''
+
+'''a = [10,20,30,40,50,"code"]
+a.extend("code")
+print(a)'''
+
+'''a = [5,7,9,10,11,13]
+a.insert(5,12)
+print(a)'''
+
+'''a = [9,7,4,0,1,5,10,8,6,3]
+a.sort()
+print(a)'''
+
+'''a = ("apple","banana","mango")
+b = list(a)
+b.append("grapes")
+print(b)
+c = tuple(b)
+print(c)
+print(type(c))'''
+
+'''id_no = int(input("enter id_no"))
+name = str(input("enter students name"))
+mobile =input("enter mobile number")
+mail = str(input("enter students mail id"))
+collage_name = str(input("enter collage_name"))
+branch = str(input("enter branch"))
+print("...........student profile...........")
+print("Id no is",id_no)
+print("name is",name)
+print("mobile is",mobile)
+print("mail is",mail)
+print("collage_name is",collage_name)
+print("branch is",branch)'''
