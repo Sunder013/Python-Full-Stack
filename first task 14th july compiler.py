ba71c96a7a84a682268c59ff8ce4045f6f2ae647
Akula Syam Sunder <117805550+Sunder013@users.noreply.github.com>
2026-07-15 14:08:19 +0530

    Add files via upload

---
+++ first task 14th july compiler.py
@@ -0,0 +1,32 @@
+'''a = int(input("enter the value"))
+b = int(input("enter the value"))
+print(a+b)'''
+
+'''a = float(input("enter the a value"))
+b = float(input("enter the b value"))
+print(a+b)'''
+
+'''a = str(input("data 1"))
+b = str(input("data 2"))
+print(a+b)'''
+
+'''a = input("data 1")
+b = input("data 2")
+print(a+b)'''
+
+'''fname = input("first name")
+lname = input("last name")
+print((fname+" "+lname).title())'''
+
+'''city = input("city name")
+print(city)'''
+
+'''a = complex(input("enter the a value"))
+b = complex(input("enter the b value"))
+print(a+b)'''
+
+'''a = bool(input("enter the a value"))
+b = bool(input("enter the b value"))
+c = bool(input("enter the c value"))
+print(a+b+c)'''
+
