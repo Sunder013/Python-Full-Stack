a7fdacf4bc5dfb0a5f5b685fc787164503f95abd
Akula Syam Sunder <117805550+Sunder013@users.noreply.github.com>
2026-07-17 14:07:19 +0530

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,207 @@
+#if - condition by using comparision operators
+#<,>,<=,>=,!=,==
+'''a = 10
+b = 15
+if a<b:
+    print("true")'''
+
+'''a = 6
+b = 9
+if a>b:
+    print("true")'''
+
+'''a = 12
+b = 14
+if a>b:
+    print("true")'''
+
+'''a = 12
+b = 14
+if a<=b:
+    print("true")'''
+
+'''a = 15
+b = 12
+if a>=b:
+    print("true")'''
+
+'''a = 10
+b = 15
+if a>=b:
+    print("greater")'''
+
+'''a = 20
+b = 40
+if a!=b:
+    print("not equal")'''
+
+'''a = 5
+b = 5
+if a ==b:
+    print("equal")'''
+
+'''a = 9
+if a>4:
+    print("greater")'''
+
+'''a = 5
+b =5
+if a == b:
+    print("equal")'''
+
+'''a = "python"
+b = "java"
+if a == b:
+    print("equal")'''
+
+'''a = "python"
+if a!="java":
+    print("equal to")'''
+
+'''a = int(input("a value"))
+b = int(input("b value"))
+if a<b:
+    print("less")'''
+
+'''a = int(input("a value"))
+if a>40:
+    print("greater")'''
+
+'''a = input("data")
+if a == "python":
+    print("true")'''
+
+#Logical operators
+# and, or, not
+
+'''a = 5
+b = 7
+if a<b and b>a:
+    print("true")'''
+
+'''a = 7
+b = 9
+if a>b and b<a:
+    print("true")'''
+
+'''a = 4
+b = 6
+if a!=b and a == b:
+    print("false")'''
+
+'''a = 4
+b = 7
+if a!= b or a == b:
+    print("true")'''
+
+'''a = 5
+b = 9
+if not a>b:
+    print("True")'''
+
+'''a = 6
+b = 12
+if not b>a:
+    print("true")'''
+
+'''a = 5
+b = 9
+if a<b and b>a:
+    print("true")'''
+
+'''a = 17
+b = 26
+if a!=b or a>b:
+    print("true")'''
+
+'''a = 50
+b = 90
+if not a<b or b>a:
+    print("true")'''
+#is, is not
+
+'''a = 7
+if type(a) is int:
+    print("it is not")'''
+
+'''a = 9
+if type(a) is not int:
+    print("ït is not int")'''
+
+'''a = int(input("a value"))
+if type(a) is int:
+    print("it is int")'''
+
+'''a = 7.5
+if type(a) is float:
+    print("true")'''
+
+'''a = int(input("ënter a value"))
+if type(a) is not float:
+    print("is not float")'''
+
+#in, not in
+'''a =2,3,4,5,6,7,8,9,10
+if 10 in a:
+    print("true")'''
+
+'''a = 2,3,4,5,6,7,8,9,10
+if 10 not in a:
+    print("false")'''
+
+'''a = 2,3,4,5,7,8,9,15
+if 20 not in a:
+    print("true")'''
+
+'''a = [2,3,4,5,6,7,8,9,10]
+b = int(input("enter a value"))
+if b in a:
+    print("true")'''
+
+'''a = int(input("enter a value"))
+if 30 in a:
+    print("true")# error'''
+
+#if - else conditions by using comparision operators
+
+'''a = 4
+b = 6
+if a<b:
+    print("less")
+else:
+    print("true")'''
+
+'''a = 3
+b = 4
+if a>b:
+    print("less")
+else:
+    print("true")'''
+
+'''a = 3
+b = 6
+if a>b:
+    print("less")
+else:
+    print("true")'''
+
+'''a = 4
+b = 9
+if a!=b or a == b:
+    print("true")'''
+ 
+    
+    
+
+
+
+              
+
+
+
+    
+
+    
+    
+    
+
