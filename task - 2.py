27a06884cfa3fb5dd9429bd22731e186ab388691
Akula Syam Sunder <117805550+Sunder013@users.noreply.github.com>
2026-07-18 15:27:31 +0530

    Add files via upload

---
+++ task - 2.py
@@ -0,0 +1,75 @@
+#nested if
+'''username = input("enter the username")
+password = int(input("enter the password"))
+if username =="shyam":
+    if password ==1234:
+        print("login successful")
+else:
+    print("invalid credentials")'''
+
+
+'''username = input("enter the username")
+password = int(input("enter the password"))
+if username =="shyam":
+    if password ==1234:
+        print("login successful")
+else:
+    print("invalid credentials")
+    if username not in username:
+        print("invalid username")'''
+
+#multiple if - task
+
+"""a = int(input("enter the age"))
+b = int(input("enter the marks"))
+c = int(input("enter the attendence"))
+if a>=18:
+    print("eligible for vote")
+else:
+    print("not eligible for vote")
+if b>=70:
+    print("allow to write exams")
+else:
+    print("not eligible to write exams")
+if c>=80:
+    print("they are eligible for scholarship")
+else:
+    print("not eligible for scholarship")"""
+
+#if - elif - else task
+
+'''cake=input("enter the cakename")
+if cake=="red velvet":
+            print(1200)
+elif cake=="choco almond":
+        print(1000)
+elif cake=="honey almond":
+    print(800)
+elif cake=="butterscotch":
+    print(600)
+else:
+    print("cake is not available")'''
+
+'''pizza = int(input("enter the price"))
+if pizza == 1000:
+    print("bbq pizza")
+elif pizza == 800:
+    print("crispy chicken pizza")
+elif pizza == 600:
+    print("paneer pizza")
+elif pizza == 400:
+    print("corn pizza")
+elif pizza == 200:
+    print("french fries & coke")
+else:
+    print("item not available")'''
+
+
+
+
+
+
+
+            
+
+
