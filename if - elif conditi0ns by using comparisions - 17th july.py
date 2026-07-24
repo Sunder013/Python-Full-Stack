dd154ccfaf477ef77010d1897c337c8dfbbf7f6a
Akula Syam Sunder <117805550+Sunder013@users.noreply.github.com>
2026-07-24 16:03:12 +0530

    Add files via upload

---
+++ if - elif conditi0ns by using comparisions - 17th july.py
@@ -0,0 +1,193 @@
+#if -elif conditions by using comparision
+
+'''a = 2
+b = 4
+if a<b:
+    print("less")
+elif b>a:
+    print("greater")'''
+
+'''a = 2
+b = 4
+if a ==b:
+    print("equal")
+elif a<b:
+    print("not equal")'''
+
+'''a = 4
+b = 8
+if a<b:
+    print("equal")
+elif a>b:
+    print("greater")
+elif a!=b:
+    print("not equal")'''
+
+'''a = 5
+b = 10
+if a>b:
+    print("greater")
+elif a!=b:
+    print("not equal")
+else:
+    print("true")'''
+
+'''a = 5
+b = 9
+if a>b and b<a:
+    print("greater")
+elif a!=b:
+    print("not equal")
+else:
+    print("less")'''
+
+'''a = int(input("a value"))
+if type(a) is int:
+    print("it is int")
+else:
+    print("it is not int")'''
+
+#
+
+'''a = 9
+b = 11
+if a>b:
+    print("less")
+if a<b:
+    print("greater")
+if a==b:
+    print("not equal")'''
+
+'''a = 6
+b = 12
+if a>b:
+    print("greater")
+if a<b:
+    print("it is equal")
+if a!=b:
+    print("it is not equal")'''
+
+'''a = 9
+b = 18
+if a==b:
+    print("it is equal")
+if a!=b:
+    print("it is not equal")'''
+
+#Nested if
+
+'''a = 2
+b = 4
+if a<b:
+    print("less")
+    if b>a:
+        print("greater")'''
+
+'''a = 2
+b = 4
+if a==b:
+    print("less")
+    if b>a:
+        print("greater")'''
+
+'''a = 10
+b = 20
+if a<b:
+    print("less")
+    if b<a:
+        print("greater")'''
+
+'''a = 15
+b = 13
+if a<b:
+    print("equal")
+    if a>b:
+        print("it is equal")'''
+
+'''a = 15
+b = 20
+if a<b:
+    print("less")
+    if b<a:
+        print("greater")
+    else:
+        print("true")'''
+
+'''a = 2
+b = 4
+if a<b:
+    print("less")
+    if b>a:
+        print("greater")
+    elif a!=b:
+        print("not equal")'''
+
+#tasks- if, if else
+
+'''a = int(input())
+if a>=18:
+    print("eligible for vote")
+else:
+    print("not eligible for vote")'''
+
+# even or odd
+
+'''num = int(input("enter the number"))
+if num%2==0:
+    print("it is even")
+else:
+    print("it is odd")'''
+
+# leap year
+'''num = int(input("enter the year"))
+if num%4==0:
+     print("it is leap year")
+else:
+    print("it is not a leap year")'''
+
+# string tasks
+
+'''a = str(input("enter the name").lower())
+if type(a) is str:
+    print("welcome shyam")
+else:
+    print("welcome guest")'''
+
+'''str = ["shyam","sunder","sai","rahul","janardhan"]
+a = input("enter the name").lower()
+if a in str:
+    print("welcome",a)
+else:
+    print("welcome guest")'''
+
+
+    
+'''name = input("enter the name")
+if name =="shyam":
+    print("welcome",name)
+else:
+    print("welcome guest")'''
+
+'''str = ["a","e","i","o","u"]
+a = input("enter the letter")
+if a in str:
+    print("it is vowel")
+else:
+    print("it is consonant")'''
+
+"""a = ["a","e","i","o","u"]
+print(input(a))"""
+
+
+          
+
+
+        
+        
+        
+        
+    
+    
+
+
+    
---
+++ loops programmes - 18th july.py
@@ -0,0 +1,54 @@
+#in, tuple, set and dictionary in loops
+#for,while,range,break,continue,pass
+#for loop()
+'''a = [10,20,30,40,50]
+for i in a:
+    print(i)'''
+
+'''a = [10,20,30,40,50]
+for i in a:
+    print(a)'''
+
+'''a = [10,20,30,40,50]
+for i in a:
+    print(i,end=",")'''
+
+'''a = [10,20,30,40,50]
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+'''b = (5,6,7,8,9)
+for i in b:
+    print(i)
+print(type(b))
+print(type(i))'''
+
+'''a = {"name":"shyam","city":"vijayawada","state":"andhra pradesh","year":2026}
+for i in a:
+    print(i)
+for i in a.keys():
+    print(i)
+    print(type(a))
+    print(type(i))
+for i in a.items():
+    print(i)
+    print(type(a))
+    print(type(i))
+for i in a.values():
+    print(type(a))
+    print(type(i))'''
+
+'''b = "codegnan"
+for i in b:
+    print(i)'''
+
+'''c = [5.6,7.8]
+for i in c:
+    print(i)
+    print(type(c))
+    print(type(i))'''
+    
+    
+
