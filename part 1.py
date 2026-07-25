c769414ec9d8fd0c5b0b4862729744d29843d953
Akula Syam Sunder <117805550+Sunder013@users.noreply.github.com>
2026-07-25 10:40:52 +0530

    Add files via upload

---
+++ part 1.py
@@ -0,0 +1,119 @@
+#for loop - sequence iteration
+'''a = ["python","java","dsa"]
+b = []
+for i in a:
+    b.append(i.upper())
+print(b)'''
+
+'''a = ["python","java","dsa"]
+for i in a:
+    a.append(i.upper())
+print(a)'''
+
+#while loop() - unlimited iterations like infinity
+'''a = 10
+while a>1:
+    print(a)'''
+
+'''a = 10
+while a<1:
+    print(a)'''
+
+'''a = 10
+while a>1:
+    print(a)
+    a=a-1'''
+
+'''a = 15
+while a<2:
+    print(a)
+    a=a-1'''
+
+'''a = 12
+while a>10:
+    print(a)
+    a=a-3'''
+
+'''a = 20
+while a>2:
+    print(a)
+    a=a-1'''
+
+'''a = 20
+while a>2:
+    a=a-1
+print(a)'''
+
+'''a = 30
+while a>5:
+    print(a)
+    a+=1'''
+
+'''a = 30
+while a>5:
+    print(a)
+    a-=1'''
+
+'''a = 1
+while a<30:
+    print(a)
+    a+=1'''
+
+'''while True:
+    age = int(input("enter the age"))
+    if age>=18:
+        print("eligible to vote")
+    else:
+        print("ineligible to vote")'''
+
+#range()
+'''the range function returns it sequence of numbers, starting from zero by default and increments by one by one and stops before a specified number.
+3 steps'''
+#start-stop-step
+
+'''for i in range(11):
+    print(i)'''
+
+'''for i in range(5,26):
+    print(i)'''
+
+'''for i in range(19):
+    print(i)'''
+
+'''for i in range(0,19):
+    print(i)'''
+
+'''for i in range(0,19,5):
+    print(i)'''
+
+'''for i in range(0,20,10):
+    print(i)'''
+
+'''for i in range(5,50,5):
+    print(i)'''
+
+'''for i in range(3,30,3):
+    print(i)'''
+
+'''while True:
+    marks = int(input("enter the marks"))
+    if marks in range(90,101):
+        print("grade - a")
+    elif marks in range(81,91):
+        print("grade - b")
+    elif marks in range(71,81):
+        print("grade - c")
+    elif marks in range(50,71):
+        print("grade - d")
+    else:
+        print("fail,study well....")''' 
+        
+
+    
+    
+    
+
+
+
+
+
