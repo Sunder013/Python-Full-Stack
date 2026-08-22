539ed624a75ca652e5390c5c19cab3212a474add
Akula Syam Sunder <117805550+Sunder013@users.noreply.github.com>
2026-08-22 10:40:24 +0530

    Add files via upload

---
+++ mymodule - day - 24.py
@@ -0,0 +1,44 @@
+# mymodule
+
+'''if __name__ == "__main__":
+    a = [10,20,30,40,50]
+    a.append("code")
+    a.extend("code")
+    print(a)'''
+
+'''def dummy():
+    if __name__=="__main__":
+        print("this pogram is run as script")
+    else:
+        print("this program is run as module")
+dummy()'''
+
+
+#math module
+'''import math
+print(math.pi)
+print(math.pi*2)
+print(math.pi*3)
+print(math.sqrt(2))
+print(math.pow(2,4))
+print(math.log(2))
+print(math.tan(45))
+print(math.sin(60))
+print(math.cos(30))
+print(math.ceil(2.9))
+print(math.ceil(5.9))
+print(math.ceil(8))
+print(math.floor(2.7))
+print(math.acos(1))
+print(math.factorial(5))'''
+
+#from keyword
+'''from math import pi,log,sqrt
+print(pi)
+print(log(9))
+print(sqrt(2))'''
+
+#system module
+import sys
+print(sys.path)
+print(sys.version)
